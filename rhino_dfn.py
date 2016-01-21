badccd69a92380ca8239d9b5f59821e4107e79bb
sunshinegeology <sunshine.abbott@gmail.com>
2016-01-21 17:40:40 +0000

    init python driver

--- rhino_dfn.py
+++ rhino_dfn.py
@@ -1,11 +1,74 @@
 import Rhino as rh
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
+import json
+import copy
+
+
+def update_views():
+    """Applies rendering and redraws all objects."""
+    #[rs.ViewDisplayMode(view, 'Ghosted') for view in rs.ViewNames()]
+    sc.doc.Views.Redraw()
+
+
+def document():
+    """Brute-force new document, discard all unsaved changes."""
+    rs.DocumentModified(False)
+    rs.Command('_-New _None')
+    update_views()
+
+
+def surf(cpts):
+    """Creates a surface based on points list, returns GUID.""" 
+    return rs.AddSrfPt(cpts)
+    
+    
+def rect_corner_pts(midpt, edge_length, normal='x'):
+    """
+    Returns list of cornerpoints of a rectangle with midpoint,
+    edge_length and normal to x/y/z axis in no particular order.
+    """
+    aidcs = range(3)
+    del aidcs[['x', 'y', 'z'].index(normal)]
+    cpts = [copy.deepcopy(midpt) for i in range(4)]
+    sgns, isgns = [1.,1.,-1.,1.,-1.,-1.,1.,-1.], 0
+    for i in range(4):
+        for j in aidcs:
+            cpts[i][j] += sgns[isgns]*edge_length/2.
+            isgns += 1
+    return cpts
+    
+    
+def layer(lname):
+    """Changes to given layer, creates if not yet existent."""
+    if not rs.IsLayer(lname):
+        rs.AddLayer(lname) 
+    rs.CurrentLayer(lname)
+
+
+def cube(edge_length, prefix='', midpt=(0,0,0)):
+    midpts = [rh.Geometry.Point3d(midpt[0]-edge_length/2., midpt[1], midpt[2]),
+              rh.Geometry.Point3d(midpt[0]+edge_length/2., midpt[1], midpt[2]),
+              rh.Geometry.Point3d(midpt[0], midpt[1]-edge_length/2., midpt[2]),
+              rh.Geometry.Point3d(midpt[0], midpt[1]+edge_length/2., midpt[2]),
+              rh.Geometry.Point3d(midpt[0], midpt[1], midpt[2]-edge_length/2.),
+              rh.Geometry.Point3d(midpt[0], midpt[1], midpt[2]+edge_length/2.),]
+    normals = ['x', 'x', 'y', 'y', 'z', 'z']
+    layers = ['LEFT', 'RIGHT', 'FRONT', 'BACK', 'BOTTOM', 'TOP']
+    if prefix:
+        layers = [l+prefix for l in layers]
+    for i in range(6):
+        layer(layers[i])
+        cpts = rect_corner_pts(midpts[i], edge_length, normal=normals[i])
+        surfid = surf(cpts)
 
 
 def create_dfn(settings):
-    pass
-    
+    document()
+    cube(settings['outer edge length'])
+    cube(settings['outer edge length']-2*settings['rmax'], '_INT')
+
+
 if __name__ == '__main__':
     with open('rhino_settings.json', 'r') as f:
         settings = json.load(f)
